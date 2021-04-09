c9ff1a7cd98c5fec4f311e186dc4a32f8d9b0325
Craig Sloggett <craigsloggett@gmail.com>
2021-04-09 15:22:32 -0400

    updates

--- config/generate-config.sh
+++ config/generate-config.sh
@@ -4,13 +4,15 @@
 
 etcd_server_ip="192.168.1.110"
 api_server_ip="192.168.1.110"
-pod_ip_range="10.200.0.0/16"
-service_ip_range="10.32.0.0/24"
+pod_cidr="10.200.0.0/16"
+service_cidr="10.32.0.0/24"
 
 config_directory="/etc/kubernetes"
 pki_directory="/etc/kubernetes/pki"
 etcd_tls_directory="/etc/etcd/tls"
 kubeconfig_directory="/etc/kubernetes/kubeconfig"
+
+node_hostnames=("node-0" "node-1" "node-2")
 
 # ---
 
@@ -49,7 +51,7 @@ cat > kube-apiserver.service <<- EOF
 	  --service-account-key-file=${pki_directory}/service-account.pem \\
 	  --service-account-signing-key-file=${pki_directory}/service-account-key.pem \\
 	  --service-account-issuer=api \\
-	  --service-cluster-ip-range=${service_ip_range} \\
+	  --service-cluster-ip-range=${service_cidr} \\
 	  --service-node-port-range=30000-32767 \\
 	  --tls-cert-file=${pki_directory}/kubernetes.pem \\
 	  --tls-private-key-file=${pki_directory}/kubernetes-key.pem \\
@@ -73,7 +75,7 @@ cat > kube-controller-manager.service <<- EOF
 	[Service]
 	ExecStart=/usr/local/bin/kube-controller-manager \\
 	  --bind-address=0.0.0.0 \\
-	  --cluster-cidr=${pod_ip_range} \\
+	  --cluster-cidr=${pod_cidr} \\
 	  --cluster-name=kubernetes \\
 	  --cluster-signing-cert-file=${pki_directory}/ca.pem \\
 	  --cluster-signing-key-file=${pki_directory}/ca-key.pem \\
@@ -82,7 +84,7 @@ cat > kube-controller-manager.service <<- EOF
 	  --root-ca-file=${pki_directory}/ca.pem \\
 	  --node-cidr-mask-size=23 \\
 	  --service-account-private-key-file=${pki_directory}/service-account-key.pem \\
-	  --service-cluster-ip-range=${service_ip_range} \\
+	  --service-cluster-ip-range=${service_cidr} \\
 	  --use-service-account-credentials=true \\
 	  --v=2
 	Restart=on-failure
@@ -126,48 +128,46 @@ EOF
 # Kubernetes Kubelet
 
 cat > kubelet.service <<- EOF
-[Unit]
-Description=Kubernetes Kubelet
-Documentation=https://github.com/kubernetes/kubernetes
-
-[Service]
-ExecStart=/usr/local/bin/kubelet \\
-  --config=/etc/kubernetes/kubelet-config.yaml \\
-  --cgroup-driver=systemd \\
-  --container-runtime=remote \\
-  --container-runtime-endpoint='unix:///var/run/crio/crio.sock' \\
-  --kubeconfig=/etc/kubernetes/kubeconfig/kubelet.kubeconfig \\
-  --v=2
-Restart=on-failure
-RestartSec=5
-
-[Install]
-WantedBy=multi-user.target
 	[Unit]
 	Description=Kubernetes Kubelet
 	Documentation=https://github.com/kubernetes/kubernetes
 	
 	[Service]
-	ExecStart=/usr/bin/kubelet \\
-	  --allow-privileged=true \\
-	  --api-servers=https://10.0.1.94:6443,https://10.0.1.95:6443,https://10.0.1.96:6443 \\
-	  --cloud-provider= \\
-	  --cluster-dns=10.32.0.10 \\
-	  --cluster-domain=cluster.local \\
-	  --configure-cbr0=true \\
-	  --container-runtime=docker \\
-	  --docker=unix:///var/run/docker.sock \\
-	  --network-plugin=kubenet \\
-	  --kubeconfig=/var/lib/kubelet/kubeconfig \\
-	  --reconcile-cidr=true \\
-	  --serialize-image-pulls=false \\
-	  --tls-cert-file=/var/lib/kubernetes/kubernetes.pem \\
-	  --tls-private-key-file=/var/lib/kubernetes/kubernetes-key.pem \\
+	ExecStart=/usr/local/bin/kubelet \\
+	  --config=/etc/kubernetes/kubelet-config.yaml \\
+	  --container-runtime=remote \\
+	  --container-runtime-endpoint='unix:///var/run/crio/crio.sock' \\
+	  --kubeconfig=/etc/kubernetes/kubeconfig/kubelet.kubeconfig \\
 	  --v=2
-	  
 	Restart=on-failure
 	RestartSec=5
 	
 	[Install]
 	WantedBy=multi-user.target
 EOF
+
+for node_hostname in "${node_hostnames[@]}"; do
+	cat > ${node_hostname}-kubelet-config.yaml <<- EOF
+		kind: KubeletConfiguration
+		apiVersion: kubelet.config.k8s.io/v1beta1
+		authentication:
+		  anonymous:
+		    enabled: false
+		  webhook:
+		    enabled: true
+		  x509:
+		    clientCAFile: "${pki_directory}/ca.pem"
+		authorization:
+		  mode: Webhook
+		cgroupDriver: "systemd"
+		clusterDomain: "cluster.local"
+		clusterDNS:
+		  - "10.32.0.10"
+		podCIDR: "${pod_cidr}"
+		resolvConf: "/run/systemd/resolve/resolv.conf"
+		runtimeRequestTimeout: "15m"
+		tlsCertFile: "${pki_directory}/${node_hostname}.pem"
+		tlsPrivateKeyFile: "${pki_directory}/${node_hostname}-key.pem"
+	EOF
+done
+
